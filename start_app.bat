61de15508bbcee272385502a5cbe58384ac5bb44
zbmen-SMCovered <zbmen@smcovered.com>
2024-03-20 13:03:39 -0700

    修改启动项

--- start_app.bat
+++ start_app.bat
@@ -4,5 +4,17 @@ git pull
 REM CALL conda.bat install --file requirements.txt
 REM 或者使用以下命令如果使用 pip
 CALL python -m pip install -r requirements.txt
-python app.py
+
+REM 检查Loggings.txt的大小，如果大于5MB，则删除并创建新的日志文件
+if exist Loggings.txt (
+    FOR %%A IN (Loggings.txt) DO (
+        set size=%%~zA
+        if %%~zA GTR 5120000 (
+            del Loggings.txt
+        )
+    )
+)
+
+REM 将输出和错误重定向到同一文件
+python app.py >> Loggings.txt 2>&1
 pause
